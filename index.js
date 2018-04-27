28ab35976192ca5804bf53e56332b786a7201429
devcarlan <devcarlan@gmail.com>
2018-04-26 20:42:16 -0400

    added command handler

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const botconfig = require("./botconfig.json");
 const tokenfile = require("./token.json");
 const Discord = require("discord.js");
-
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0) {
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded.`);
+    bot.commands.set(props.help.name, props);
+  });
+
+});
+
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
@@ -21,6 +41,9 @@ bot.on("message", async message => {
   if(cmd === `${prefix}hello`) {
     return message.channel.send("Hello!");
   }
+
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot, message, args);
 
   if(cmd === `${prefix}info`) {
     let icon = bot.user.iconURL;
@@ -121,6 +144,9 @@ bot.on("message", async message => {
 
     return;
   }
+
+
+
 
 });
 
